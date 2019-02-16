431c96065e8e18a4901801eea660fca4a4ed1c3b
Jason <quietjason@gmail.com>
2019-02-16 21:24:43 +0800

    Add app.vue

---
+++ component/single-file-component/app.vue
@@ -0,0 +1,20 @@
+<template>
+  <p>{{ greeting }} world.</p>
+</template>
+
+<script>
+  module.exports = {
+    data: function() {
+      return {
+        greeting: 'Hello'
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  p {
+    font-size: 1.2em;
+    text-align: center;
+  }
+</style>
